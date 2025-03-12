d5424b4f0ac3b7ab8f8047999fffc6ba5fc7ca9e
Victor Van Weyenberg <victorvanweyenb@gmail.com>
2025-03-12 17:41:24 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use axum::Router;
+use axum::routing::get;
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(root));
+
+    let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn root() -> &'static str {
+    "Hello World"
+}
